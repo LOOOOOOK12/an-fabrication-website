1c89d9dfb80c7bd3a0bf629ad517d3a693314e9c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-20 21:04:26 +0800

    Add HeroSection component with basic structure and styling

---
+++ app/components/sections/heroSection.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+function heroSection() {
+    return (
+        <section className='h-screen flex items-center justify-center text-3xl' id='Hero'>
+            <h1>HERO</h1>
+        </section>
+    )
+}
+
+export default heroSection
