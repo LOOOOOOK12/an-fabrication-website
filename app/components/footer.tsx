87c13d171e6192eaf4ebf3b278168872a8b8b006
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-20 21:04:08 +0800

    Add Footer component with basic styling

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <footer className='w-full flex items-center justify-center bg-slate-800 text-white p-5'>
+            <h1>FOOTER</h1>
+        </footer>
+    )
+}
+
+export default footer
