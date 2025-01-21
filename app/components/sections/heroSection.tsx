d3117e734dc86be993f25a5bcd5e7f34e586ce09
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-21 17:46:31 +0800

    Rename section ID from 'Hero' to 'Home' for consistency in the layout

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function heroSection() {
     return (
-        <section className='h-screen flex items-center justify-center text-3xl' id='Hero'>
+        <section className='h-screen flex items-center justify-center text-3xl' id='Home'>
             <h1>HERO</h1>
         </section>
     )
