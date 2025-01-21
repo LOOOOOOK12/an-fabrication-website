4a50261490c6fbfae32ef224f85ae06d051ee408
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-21 17:46:43 +0800

    Add gallery page with GallerySection and TestSection components

---
+++ app/gallery/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GallerySection from '@/app/gallery/sections/gallerySection' 
+import TestSection from '@/app/gallery/sections/testSection' 
+
+function page() {
+    return (
+        <>
+            <GallerySection/>
+            <TestSection/>
+        </>
+    )
+}
+
+export default page
