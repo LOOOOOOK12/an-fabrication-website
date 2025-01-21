773604d3b7f0d324a7b9ad57b457d595e3cb7af9
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-21 17:46:52 +0800

    Add gallerySection component for the gallery page

---
+++ app/gallery/sections/gallerySection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function gallerySection() {
+    return (
+        <div className='h-screen items-center justify-center flex' id='Home'>
+            gallerySection
+        </div>
+    )
+}
+
+export default gallerySection
