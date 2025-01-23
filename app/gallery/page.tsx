4c9b2ee8ba563577113a62c8bdadd1f63b9674ba
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-23 18:30:48 +0800

    Comment out TestSection component in gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import GallerySection from '@/app/gallery/sections/gallerySection' 
-import TestSection from '@/app/gallery/sections/testSection' 
+import TestSection from '@/app/gallery/sections/testSection'
 
 function page() {
     return (
         <>
             <GallerySection/>
-            <TestSection/>
+            {/* <TestSection/> */}
         </>
     )
 }
