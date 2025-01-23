abdc323641ce869a7190f05c704f88d4eb1c836b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-23 18:30:19 +0800

    Refactor navigation component to use dynamic navLinks and simplify layout

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,22 +1,22 @@
 "use client"
-
-import Link from 'next/link'
 import { motion } from 'motion/react'
 import React from 'react'
+import  {navLinks} from '../types/types'
 
 function nav() {
     return (
         <motion.header 
             initial={{opacity: 0, y: -100 }}
             animate={{opacity: 1, y: 0 }}
-            transition={{ duration: 1.5, type:"spring", delay: .5 }}
-            className='w-full fixed top-0 z-50 bg-slate-500 shadow-md bg-opacity-75 backdrop-blur'
+            // transition={{ duration: .5, type:"spring", delay: .5 }}
+            className='w-full fixed flex items-center justify-center top-2 z-50 bg-opacity-75'
         >
-            <nav className='w-full flex items-center justify-between p-5'>
-                <a href='#Home'>A&N</a>
-                <div>
-                    <Link href={`/`}>Home</Link>
-                    <Link href={`/gallery`}>Gallery</Link>
+            <nav className='w-1/2 bg-slate-400 flex items-center justify-between p-5'>
+                <a className='cursor-default text-xl font-semibold'>A&N</a>
+                <div className='flex gap-2'>
+                    {navLinks.map((link,idx) => 
+                        <a key={idx} href={link.href}>{link.name}</a>
+                    )}
                 </div>
             </nav>
         </motion.header>
