2da455a49eb0609b768e412c8a17e06d12be1ead
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-23 18:30:54 +0800

    Refactor gallerySection component to enhance layout and styling

--- app/gallery/sections/gallerySection.tsx
+++ app/gallery/sections/gallerySection.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 function gallerySection() {
     return (
-        <div className='h-screen items-center justify-center flex' id='Home'>
-            gallerySection
-        </div>
+        <section className='h-screen flex items-center justify-center p-6' id='Home'>
+            <div className='w-full'>
+                <h1 className='text-3xl'>Wood works</h1>
+                <div className='grid grid-cols-5 grid-rows-2 gap-2'>
+                    <div className='bg-slate-500 w-full h-32'></div>
+                    <div className='bg-slate-600 w-full h-32'></div>
+                    <div className='bg-slate-900 w-full h-32'></div>
+                    <div className='bg-slate-300 w-full h-32'></div>
+                    <div className='bg-slate-600 w-full h-32'></div>
+
+                    <div className='bg-red-400 w-full h-32'></div>
+                    <div className='bg-red-600 w-full h-32'></div>
+                    <div className='bg-red-900 w-full h-32'></div>
+                    <div className='bg-red-300 w-full h-32'></div>
+                    <div className='bg-red-600 w-full h-32'></div>
+                </div>
+            </div>
+        </section>
     )
 }
 
