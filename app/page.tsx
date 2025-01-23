e21813adf22868f694804cdde31e076f9c92d5a3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-23 18:30:15 +0800

    Add AboutSection and OffersSection components to the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./components/sections/heroSection";
+import AboutSection from "./components/sections/aboutSection"
+import OffersSection from "./components/sections/offersSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <AboutSection/>
+      <OffersSection/>
     </>
   );
 }
