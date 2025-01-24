f618b5579bf66422434ee93ac3a8360929d7d80e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-24 17:13:20 +0800

    Refactor aboutSection component to include CarouselContainer and enhance layout for better responsiveness

--- app/components/sections/aboutSection.tsx
+++ app/components/sections/aboutSection.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import CarouselContainer from '../carouselContainer'
+import { AboutCarouselData } from '../../types/types'
 
 function aboutSection() {
     return (
-        <section className='h-screen flex items-center gap-2 justify-center'>
-            <div className='w-1/2 flex gap-10 justify-between items-center'>
-                <div className='relative w-full h-96'>
-                    <div className='bg-red-600 size-40 w-40 absolute top-5'></div>
-                    <div className='bg-red-200 size-40 w-40 absolute top-32 left-20'></div>
-                    <div className='bg-red-900 size-40 w-44 absolute top-64 left-0'></div>
+        <section className='h-screen flex flex-col md:flex-row items-center gap-2 justify-center p-5'>
+            <div className='max-w-[1200px] h-[600px] flex flex-col md:flex-row items-center justify-between gap-4'>
+                <div className=' flex items-center justify-center'>
+                    <CarouselContainer
+                        items={AboutCarouselData}
+                    />
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className='max-h-[800px] w-fullflex flex-col gap-2'>
                     <h1 className='text-5xl font-bold'>About us</h1>
                     <p className='text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus at nesciunt ea dolorum voluptatem illo quia rem nostrum quibusdam, et doloremque unde distinctio veniam consectetur quae tempore. Facilis, nihil at?</p>
                 </div>
