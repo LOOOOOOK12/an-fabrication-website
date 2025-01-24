e1cb48c9d32a8b6ad3387ba5654491424dd75b1a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-24 17:13:00 +0800

    Add types for carousel components and define AboutCarousel data

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,10 +1,25 @@
 import { Wrench, Settings, ShoppingBasket, LucideProps } from "lucide-react"
+import Welding1 from "@/public/weldingPictures/welding1.jpg"
+import { StaticImageData } from "next/image"
 
+// Types
 export type offersCardProps = {
     icon: React.FC<LucideProps>,
     title: string,
     description: string
 }
+
+
+export type carouselProps = {
+    image: StaticImageData,
+    alt: string
+}
+
+export interface CarouselContainerProps {
+    items: carouselProps[];
+}
+
+// Datas
 export const navLinks = [
     {
         name:'Home',
@@ -33,4 +48,14 @@ export const offersData = [
         description:"Special discounts for businesses and large-scale projects."
     }
 ]
-    
+
+export const AboutCarouselData: carouselProps[] = [
+    {
+        image: Welding1,
+        alt: "Welding Picture 1"
+    },
+    {
+        image: Welding1,
+        alt: "Welding Picture 2"
+    },
+]
