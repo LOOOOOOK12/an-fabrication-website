a14e44ce92c72a63f994b4f1fc9362fa4f3c2992
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-24 17:13:12 +0800

    Update navigation component styles for improved layout and responsiveness

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -8,10 +8,10 @@ function nav() {
         <motion.header 
             initial={{opacity: 0, y: -100 }}
             animate={{opacity: 1, y: 0 }}
-            // transition={{ duration: .5, type:"spring", delay: .5 }}
+            transition={{ duration: .5, type:"spring", delay: .5 }}
             className='w-full fixed flex items-center justify-center top-2 z-50 bg-opacity-75'
         >
-            <nav className='w-1/2 bg-slate-400 flex items-center justify-between p-5'>
+            <nav className='w-[800px] bg-primary-700 flex items-center justify-between text-text-50 p-5 shadow-lg'>
                 <a className='cursor-default text-xl font-semibold'>A&N</a>
                 <div className='flex gap-2'>
                     {navLinks.map((link,idx) => 
