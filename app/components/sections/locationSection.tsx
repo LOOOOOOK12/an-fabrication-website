c402e24a4b4f6b656c7e159d2db4359fdd57e4e2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-24 17:13:32 +0800

    Add locationSection component for displaying location information

---
+++ app/components/sections/locationSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function locationSection() {
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <h1>Location</h1>
+        </div>
+    )
+}
+
+export default locationSection
