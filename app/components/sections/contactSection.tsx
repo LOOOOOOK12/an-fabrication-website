39d3f104eb20849360e2d36bc0d602311f47277f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-27 17:39:36 +0800

    Add ContactSection component for user inquiries

---
+++ app/components/sections/contactSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function contactSection() {
+    return (
+        <div className='h-screen flex items-center justify-center bg-primary-300' id='contact'>
+            <div>
+
+            </div>
+            <div>
+                <h1>Contact Us!</h1>
+                <div className=''>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default contactSection
