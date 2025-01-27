e47607d34f1ce59734e3b8a8201fdbac8ae0f3b7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-27 17:39:40 +0800

    Enhance HeroSection with logo image and updated title for better branding

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import React from 'react'
+import Image from 'next/image'
+import Logo from "@/public/logo.jpg"
 
 function heroSection(){
     return (
-        <section className='h-screen flex bg-primary-300 items-center justify-center text-3xl relative' id='Home'>
-            <div className='bg-white rounded-full flex justify-center items-center size-44'>
-                <h1>A&N</h1>
+        <section className='h-screen flex bg-primary-300 items-center justify-center text-3xl relative' id='home'>
+            <div className='flex flex-col items-center justify-center gap-4'>
+                <Image src={Logo} alt="Logo" className='size-52 rounded-full'/>
+                <h1 className='text-4xl font-bold'>Alamil and Napoles Wood & Metal Fabrication</h1>
             </div>
         </section>
     )
