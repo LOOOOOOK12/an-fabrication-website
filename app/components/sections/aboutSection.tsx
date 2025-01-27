d8872bcb956ab0bb8324402192204c8fbf2dac36
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-27 17:39:19 +0800

    Update AboutSection layout and styling for improved presentation

--- app/components/sections/aboutSection.tsx
+++ app/components/sections/aboutSection.tsx
@@ -4,16 +4,16 @@ import { AboutCarouselData } from '../../types/types'
 
 function aboutSection() {
     return (
-        <section className='h-screen flex flex-col md:flex-row items-center gap-2 justify-center p-5'>
-            <div className='max-w-[1200px] h-[600px] flex flex-col md:flex-row items-center justify-between gap-4'>
-                <div className=' flex items-center justify-center'>
+        <section className='h-screen flex flex-col md:flex-row items-center gap-2 justify-center p-5' id='about'>
+            <div className='max-w-[1000px] h-[600px] flex flex-col md:flex-row items-center justify-between gap-4'>
+                <div className='flex items-center justify-center'>
                     <CarouselContainer
                         items={AboutCarouselData}
                     />
                 </div>
-                <div className='max-h-[800px] w-fullflex flex-col gap-2'>
+                <div className='max-h-[800px] w-full flex flex-col gap-4'>
                     <h1 className='text-5xl font-bold'>About us</h1>
-                    <p className='text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus at nesciunt ea dolorum voluptatem illo quia rem nostrum quibusdam, et doloremque unde distinctio veniam consectetur quae tempore. Facilis, nihil at?</p>
+                    <p className='text-xl text-text-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus at nesciunt ea dolorum voluptatem illo quia rem nostrum quibusdam, et doloremque unde distinctio veniam consectetur quae tempore. Facilis, nihil at?</p>
                 </div>
             </div>
         </section>
