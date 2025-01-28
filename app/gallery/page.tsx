d846cb046b29e69c44adefa627493e03503f690e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-28 20:21:06 +0800

    Add navigation component to gallery page for improved user experience

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import GallerySection from '@/app/gallery/sections/gallerySection' 
 import TestSection from '@/app/gallery/sections/testSection'
+import Nav from '../components/nav'
+import { navLinksGallery } from '../types/types'
 
 function page() {
     return (
         <>
+            <Nav
+                links={navLinksGallery}
+            />
             <GallerySection/>
             <TestSection/>
         </>
