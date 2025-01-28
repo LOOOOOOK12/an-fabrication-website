deb9974af240076359046a099cc1df260c1fbaec
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-28 20:20:12 +0800

    Add Nav component and LocationSection to Home for improved navigation and layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,21 @@ import HeroSection from "./components/sections/heroSection";
 import AboutSection from "./components/sections/aboutSection"
 import OffersSection from "./components/sections/offersSection";
 import ServicesSection from "./components/sections/servicesSection";
+import LocationSection from "./components/sections/locationSection";
+import Nav from "./components/nav"
+import { navLinksHome } from "./types/types";
 
 export default function Home() {
   return (
     <>
+      <Nav
+        links={navLinksHome}
+      />
       <HeroSection/>
       <AboutSection/>
       <OffersSection/>
       <ServicesSection/>
+      <LocationSection/>
     </>
   );
 }
