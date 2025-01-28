caa43649bfefec2c7bdcdf0f3b79a369ac7d1aec
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-28 20:20:26 +0800

    Refactor Nav component to accept dynamic links and improve layout

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,9 +1,11 @@
 "use client"
+
 import { motion } from 'motion/react'
 import React from 'react'
-import  {navLinks} from '../types/types'
+import  { navLinks } from '../types/types'
+import Link from 'next/link'
 
-function nav() {
+function nav({ links }:navLinks) {
     return (
         <motion.header 
             initial={{opacity: 0, y: -100 }}
@@ -11,11 +13,11 @@ function nav() {
             transition={{ duration: .5, type:"spring", delay: .5 }}
             className='w-full fixed flex items-center justify-center top-2 z-50 bg-opacity-75'
         >
-            <nav className='w-[800px] bg-primary-700 flex items-center justify-between text-text-50 p-5 shadow-lg'>
-                <a className='cursor-default text-3xl font-semibold'>A&N</a>
-                <div className='flex gap-2'>
-                    {navLinks.map((link,idx) => 
-                        <a className='text-xl hover:text-secondary-400' key={idx} href={link.href}>{link.name}</a>
+            <nav className='w-[800px] bg-primary-700 flex items-center justify-between text-text-50 p-4 shadow-lg'>
+                <Link className='cursor-default text-3xl font-semibold' href={"/"}>A&N</Link>
+                <div className='flex gap-4'>
+                    {links.map((link, idx) => 
+                        <a className='text-xl hover:text-secondary-400 hover:duration-100' key={idx} href={link.href}>{link.name}</a>
                     )}
                 </div>
             </nav>
