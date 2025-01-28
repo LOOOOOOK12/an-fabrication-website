af29ecb36a047d25c48e2fcee35fcdb27e7cb6dd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-28 20:21:00 +0800

    Add height to servicesSection for full-screen display and include a link to the gallery

--- app/components/sections/servicesSection.tsx
+++ app/components/sections/servicesSection.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ServicesCardData } from '@/app/types/types'
 import ServicesCard from '../servicesCard'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 function servicesSection() {
     return (
-        <section className='bg-primary-50 flex flex-col items-center justify-center gap-2 p-5' id='services'>
+        <section className='h-screen bg-primary-50 flex flex-col items-center justify-center gap-6 p-5' id='services'>
             <h1 className='text-5xl font-bold'>Services</h1>
-            <div className='flex flex-wrap gap-4'>
+            <div className='flex flex-wrap items-center justify-center gap-4'>
                 {ServicesCardData.map((card, idx) => 
                     <ServicesCard
                         key={idx}
@@ -15,6 +17,7 @@ function servicesSection() {
                     />
                 )}                
             </div>
+            <Link href={"/gallery"}><Button>Gallery</Button></Link>
         </section>
     )
 }
