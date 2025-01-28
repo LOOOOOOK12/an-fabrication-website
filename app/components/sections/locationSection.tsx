24e9aa697c955ac844238ff7b6c5504830e6bd5f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-28 20:20:46 +0800

    Add height to locationSection for full-screen display

--- app/components/sections/locationSection.tsx
+++ app/components/sections/locationSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function locationSection() {
     return (
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center h-screen' id='location'>
             <h1>Location</h1>
         </div>
     )
