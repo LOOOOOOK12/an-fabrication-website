c35aaaec194969f86ce768059197c5e30e089edc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-29 18:51:32 +0800

    Refactor about section layout for improved spacing and responsiveness

--- app/components/sections/aboutSection.tsx
+++ app/components/sections/aboutSection.tsx
@@ -4,7 +4,7 @@ import { AboutCarouselData } from '../../types/types'
 
 function aboutSection() {
     return (
-        <section className='h-screen flex flex-col md:flex-row items-center gap-2 justify-center p-5' id='about'>
+        <section className='flex flex-col py-8 px-4 md:flex-row items-center gap-2 justify-center' id='about'>
             <div className='max-w-[1000px] h-[600px] flex flex-col md:flex-row items-center justify-between gap-4'>
                 <div className='flex items-center justify-center'>
                     <CarouselContainer
