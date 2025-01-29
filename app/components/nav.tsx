aeb59bb8910f33655e8edd32ecad0d1529a4181b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-29 18:51:27 +0800

    Add logo to navigation bar for enhanced branding

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,6 +4,8 @@ import { motion } from 'motion/react'
 import React from 'react'
 import  { navLinks } from '../types/types'
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '@/public/logo.jpg'
 
 function nav({ links }:navLinks) {
     return (
@@ -14,7 +16,9 @@ function nav({ links }:navLinks) {
             className='w-full fixed flex items-center justify-center top-2 z-50 bg-opacity-75'
         >
             <nav className='w-[800px] bg-primary-700 flex items-center justify-between text-text-50 p-4 shadow-lg'>
-                <Link className='cursor-default text-3xl font-semibold' href={"/"}>A&N</Link>
+                <Link className='cursor-default text-3xl font-semibold size-10' href={"/"}>
+                    <Image src={Logo} alt="Logo" className='h-full w-full rounded-full'/>
+                </Link>
                 <div className='flex gap-4'>
                     {links.map((link, idx) => 
                         <a className='text-xl hover:text-secondary-400 hover:duration-100' key={idx} href={link.href}>{link.name}</a>
