9961672a7d3766e3da909001b660abd222fda60a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-30 16:17:08 +0800

    Update ServicesSection layout and content for improved clarity and structure

--- app/components/sections/servicesSection.tsx
+++ app/components/sections/servicesSection.tsx
@@ -7,17 +7,19 @@ import Link from 'next/link'
 function servicesSection() {
     return (
         <section className='py-8 px-4 bg-primary-50 flex flex-col items-center justify-center gap-6' id='services'>
-            <h1 className='text-5xl font-bold'>Services</h1>
-            <div className='flex flex-wrap items-center justify-center gap-4'>
-                {ServicesCardData.map((card, idx) => 
-                    <ServicesCard
-                        key={idx}
-                        icon={card.icon}
-                        title={card.title}
-                    />
-                )}                
+            <div className='flex flex-col items-start justify-center gap-4 max-w-[950px]'>
+                <h1 className='text-5xl font-bold'>Services We Provide</h1>
+                <div className='flex flex-wrap items-stretch gap-4'>
+                    {ServicesCardData.map((card, idx) => 
+                        <ServicesCard
+                            key={idx}
+                            icon={card.icon}
+                            title={card.title}
+                        />
+                    )}                
+                </div>
+                <Link href={"/gallery"}><Button>View Gallery</Button></Link>
             </div>
-            <Link href={"/gallery"}><Button>View Gallery</Button></Link>
         </section>
     )
 }
