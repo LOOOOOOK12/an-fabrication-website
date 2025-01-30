63c60568ea64f82824e8c2c5f6b5412af499df5a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-30 16:17:12 +0800

    Add Footer component to Gallery page for improved navigation

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,6 +3,7 @@ import GallerySection from '@/app/gallery/sections/gallerySection'
 import TestSection from '@/app/gallery/sections/testSection'
 import Nav from '../components/nav'
 import { navLinksGallery } from '../types/types'
+import Footer from '../components/footer'
 
 function page() {
     return (
@@ -12,6 +13,9 @@ function page() {
             />
             <GallerySection/>
             <TestSection/>
+            <Footer
+                links={navLinksGallery}
+            />
         </>
     )
 }
