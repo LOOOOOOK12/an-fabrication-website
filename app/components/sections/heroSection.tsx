dd141076c6e2aa0353384c2cfc4bfb2580020414
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-01-30 16:16:55 +0800

    Adjust HeroSection styling for improved layout and responsiveness

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -3,14 +3,13 @@
 import React from 'react'
 import Image from 'next/image'
 import Logo from "@/public/logo.jpg"
-import Wallpaper from "@/public/wallpaper.jpg"
 
 function heroSection(){
     return (
         <section className={`h-screen bg-cover bg-[url(../public/wallpaper.jpg)] flex items-center justify-center text-3xl relative`} id='home'>
             <div className='flex flex-col items-center justify-center gap-4'>
-                <Image src={Logo} alt="Logo" className='size-52 rounded-full'/>
-                <h1 className='text-4xl text-center text-text-50 font-bold'>Alamil and Napoles Wood & Metal Fabrication</h1>
+                <Image src={Logo} alt="Logo" className='size-40 rounded-full'/>
+                <h1 className='text-2xl md:text-4xl text-center text-text-50 font-bold'>Alamil and Napoles Wood & Metal Fabrication</h1>
             </div>
         </section>
     )
