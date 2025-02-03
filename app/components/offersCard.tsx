ce1834081a817cc73eef2dc8e410b7dbcf510f6b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-03 17:58:30 +0800

    Refactor Icon component to accept dynamic size prop and adjust layout in offersCard for improved presentation

--- app/components/offersCard.tsx
+++ app/components/offersCard.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { offersCardProps } from '../types/types'
 import { LucideProps } from 'lucide-react'
 
-export function Icon({ icon: Icon }: { icon: React.FC<LucideProps> }) {
-    return <Icon size={45} />;
+export function Icon({ icon: Icon, size }:{ icon: React.FC<LucideProps>, size?: number }) {
+    return <Icon size={size} />;
 }
 
 function offersCard({icon, title, description}:offersCardProps) {
     return (
-        <div className='flex flex-col items-center p-8 bg-background rounded-lg max-w-[300px] shadow-md'>
-            <Icon icon={icon}/>
-            <h1 className='text-3xl font-extrabold mb-4 text-center'>{title}</h1>
+        <div className='flex flex-col items-center justify-center p-8 bg-background rounded-lg max-w-[300px] shadow-md'>
+            <Icon icon={icon} size={50}/>
+            <h1 className='text-3xl font-extrabold my-4 text-center'>{title}</h1>
             <p className='text-text-400'>{description}</p>
         </div>
     )
