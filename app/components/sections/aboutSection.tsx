e3e7810346a90a33265aca8de2ee41114ed943ea
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-03 17:58:48 +0800

    Implement Swiper for about section carousel to enhance image presentation

--- app/components/sections/aboutSection.tsx
+++ app/components/sections/aboutSection.tsx
@@ -4,13 +4,25 @@ import React from 'react'
 import { AboutCarouselData } from '../../types/types'
 import Painting from "@/public/carousel/painting.jpg"
 import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function aboutSection() {
     return (
         <section className='flex item-center justify-center gap-4 p-6' id='about'>
-            <div className='flex flex-col md:flex-row items-center justify-center gap-4 max-w-[1200px]'>
-                <Image src={Painting} alt="asdsa" className='w-full h-[350px] object-contain'/>
-                <div className='flex flex-col gap-6 items-start justify-center'>
+            <div className='flex flex-col md:flex-row items-center justify-center gap-4 w-full md:max-w-[850px]'>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    loop={true}
+                    className='w-full h-[400px]'>
+                    {AboutCarouselData.map((data, idx) =>
+                        <SwiperSlide key={idx}  className='w-full h-full'>
+                            <Image src={data.image} alt={data.alt} className='w-full h-full'/>
+                        </SwiperSlide>
+                    )}
+                </Swiper>
+                <div className='flex flex-col gap-6 items-start justify-center w-full'>
                     <h1 className='text-5xl font-bold items-start'>About us</h1>
                     <p className='text-xl text-text-400'>
                         Alamil and Napoles Wood and Metal Fabrication 
