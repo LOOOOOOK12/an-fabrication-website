7273a67b885dc30b022e276320510b8de2623819
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-03 17:58:57 +0800

    Center align title and update layout in services section for improved presentation

--- app/components/sections/servicesSection.tsx
+++ app/components/sections/servicesSection.tsx
@@ -7,9 +7,9 @@ import Link from 'next/link'
 function servicesSection() {
     return (
         <section className='py-8 px-4 bg-primary-50 flex flex-col items-center justify-center gap-6' id='services'>
-            <div className='flex flex-col items-start justify-center gap-4 max-w-[950px]'>
-                <h1 className='text-5xl font-bold'>Services We Provide</h1>
-                <div className='flex flex-wrap items-stretch gap-4'>
+            <div className='flex flex-col items-center justify-center gap-4 max-w-[950px]'>
+                <h1 className='text-5xl font-bold text-center'>Services We Provide</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-3 justify-center items-center gap-4'>
                     {ServicesCardData.map((card, idx) => 
                         <ServicesCard
                             key={idx}
