44fc12993574c39a7c4954f73bfe527e3d2eae4e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-04 16:50:24 +0800

    Refactor servicesSection component: update import path and enhance card layout for improved display and interactivity

--- app/components/sections/servicesSection.tsx
+++ app/components/sections/servicesSection.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import { ServicesCardData } from '@/app/types/types'
+import { ServicesCardData } from '@/app/types/constants'
 import ServicesCard from '../servicesCard'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 
 function servicesSection() {
     return (
         <section className='py-8 px-4 bg-primary-50 flex flex-col items-center justify-center gap-6' id='services'>
             <div className='flex flex-col items-center justify-center gap-4 max-w-[950px]'>
                 <h1 className='text-5xl font-bold text-center'>Services We Provide</h1>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-3 justify-center items-center gap-4'>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center items-center gap-4'>
                     {ServicesCardData.map((card, idx) => 
                         <ServicesCard
+                            image={card.image}
                             key={idx}
-                            icon={card.icon}
                             title={card.title}
+                            href={card.href}
+                            alt={card.alt}
                         />
                     )}                
                 </div>
-                <Link href={"/gallery"}><Button>View Gallery</Button></Link>
             </div>
         </section>
     )
