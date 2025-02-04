e69fdbdc018edf9292134e93086b5270a89bad3a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-04 16:50:08 +0800

    Refactor contactSection component: enhance layout and update content for improved user engagement

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -4,16 +4,9 @@ import React from 'react'
 
 function contactSection() {
     return (
-        <div className='py-8 px-4 flex items-center justify-center bg-primary-300' id='contactus'>
-            <div>
-
-            </div>
-            <div>
-                <h1>Contact Us!</h1>
-                <div className=''>
-                    
-                </div>
-            </div>
+        <div className='py-8 px-4 flex flex-col gap-4 items-center justify-center h-screen' id='contactus'>
+            <h1 className='text-4xl md:text-6xl font-bold text-center'>Want Something?</h1>
+            <a href='mailto:lukealamil12@gmail.com' className='py-3 px-6 bg-accent-500 text-text-50 text-3xl font-semibold rounded-md hover:bg-accent-600 duration-100'>Contact Us!</a>
         </div>
     )
 }
