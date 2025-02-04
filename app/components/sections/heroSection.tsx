dcdcba59bb34f1a5f3e9f7adf43ab6739347a2e0
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-04 16:50:11 +0800

    Update heroSection component: replace logo image for improved branding

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import React from 'react'
-import Image from 'next/image'
-import Logo from "@/public/logo.jpg"
+import Image from 'next/image'  
+import Logo from "@/public/AN.jpg"
 
 function heroSection(){
     return (
