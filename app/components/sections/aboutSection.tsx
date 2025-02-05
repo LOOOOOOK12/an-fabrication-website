eb615a718069636b238870e33a39a60581a37b05
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-05 16:33:26 +0800

    Refactor aboutSection component: enhance layout and styling for improved responsiveness and visual appeal

--- app/components/sections/aboutSection.tsx
+++ app/components/sections/aboutSection.tsx
@@ -2,34 +2,35 @@
 
 import React from 'react'
 import { AboutCarouselData } from '../../types/constants'
-import Painting from "@/public/carousel/painting.jpg"
 import Image from 'next/image'
+import Shape from '../shape'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules'
+import { Autoplay, EffectFade } from 'swiper/modules'
 import 'swiper/css';
 
 function aboutSection() {
     return (
-        <section className='flex item-center justify-center p-6' id='about'>
-            <div className='flex flex-col md:flex-row items-center justify-center gap-8 w-full md:max-w-[850px]'>
+        <section className='relative flex item-center justify-center bg-slate-200 p-9' id='about'>
+            <div className='flex flex-col md:flex-row items-center justify-center gap-8 w-full max-w-[950px]'>
                 <Swiper
-                    modules={[Autoplay]}
+                    modules={[Autoplay, EffectFade]}
                     slidesPerView={1}
+                    effect={'fade'}
                     loop={true}
                     autoplay={{
                         delay: 2500,
                         disableOnInteraction: false,
                     }}
-                    className='w-full h-[400px] rounded-md'>
+                    className='w-full md:w-1/2 h-[350px] md:h-[450px] rounded-md'>
                     {AboutCarouselData.map((data, idx) =>
                         <SwiperSlide key={idx}  className='w-full h-full'>
                             <Image src={data.image} alt={data.alt} className='w-full h-full'/>
                         </SwiperSlide>
                     )}
                 </Swiper>
-                <div className='flex flex-col gap-4 items-start justify-center w-full'>
-                    <h1 className='text-5xl font-bold items-start'>About us</h1>
-                    <p className='text-xl text-text-400'>
+                <div className='flex flex-col gap-4 items-start justify-center w-full md:w-1/2'>
+                    <h1 className='text-4xl md:text-5xl font-bold items-start'>About us</h1>
+                    <p className='text-lg md:text-xl text-text-400'>
                     Alamil and Napoles crafts high-quality wood and metal products with durability and style. 
                     We create custom solutions for residential, commercial, and industrial needs.
                     </p>
