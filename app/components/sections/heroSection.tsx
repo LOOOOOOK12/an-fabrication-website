c9516e9a6261148f0336636221de1e53b86658e6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-05 16:33:41 +0800

    Refactor heroSection component: update heading styles for improved visibility and emphasis

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -9,7 +9,7 @@ function heroSection(){
         <section className={`h-screen bg-cover bg-[url(../public/wallpaper.jpg)] flex items-center justify-center text-3xl relative`} id='home'>
             <div className='flex flex-col items-center justify-center gap-4'>
                 <Image src={Logo} alt="Logo" className='size-40 rounded-full'/>
-                <h1 className='text-2xl md:text-4xl text-center text-text-50 font-bold'>Alamil and Napoles Wood & Metal Fabrication</h1>
+                <h1 className='text-2xl md:text-5xl text-center text-text-50 font-extrabold w-1/2'>Alamil and Napoles Wood & Metal Fabrication</h1>
             </div>
         </section>
     )
