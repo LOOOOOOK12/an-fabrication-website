3d652e1e521a7e19dfc9d5bd91329840421fd3dd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-05 16:33:17 +0800

    Refactor offersCard component: update typography for improved readability and visual hierarchy

--- app/components/offersCard.tsx
+++ app/components/offersCard.tsx
@@ -10,8 +10,8 @@ function offersCard({icon, title, description}:offersCardProps) {
     return (
         <div className='flex flex-col items-center p-8 bg-background rounded-lg max-w-[300px] shadow-md'>
             <Icon icon={icon} size={50}/>
-            <h1 className='text-3xl font-bold my-4 text-center'>{title}</h1>
-            <p className='text-text-400'>{description}</p>
+            <h1 className='text-2xl md:text-3xl my-4 font-extrabold text-center'>{title}</h1>
+            <p className='text-lg md:text-2xl text-text-400'>{description}</p>
         </div>
     )
 }
