48604bab62c6d21e24fcf1a940a20b3024b7419b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-07 16:23:16 +0800

    Update heroSection component: change background image and adjust logo and heading sizes for better responsiveness

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -6,10 +6,10 @@ import Logo from "@/public/AN.jpg"
 
 function heroSection(){
     return (
-        <section className={`h-screen bg-cover bg-[url(../public/wallpaper.jpg)] flex items-center justify-center text-3xl relative`} id='home'>
+        <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-300 bg-center bg-blend-multiply flex items-center justify-center text-3xl relative`} id='home'>
             <div className='flex flex-col items-center justify-center gap-4'>
-                <Image src={Logo} alt="Logo" className='size-40 rounded-full'/>
-                <h1 className='text-2xl md:text-5xl text-center text-text-50 font-extrabold w-1/2'>Alamil and Napoles Wood & Metal Fabrication</h1>
+                <Image src={Logo} alt="Logo" className='size-28 md:size-48 rounded-full'/>
+                <h1 className='text-2xl md:text-4xl text-center text-text-50 font-extrabold w-full md:w-3/4'>Alamil and Napoles Wood & Metal Fabrication</h1>
             </div>
         </section>
     )
