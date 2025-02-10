8f16f15e8abdf0504e2763fc6e89c7697c54b6c4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-10 16:39:50 +0800

    Update heroSection component: change background color and adjust gap for improved layout

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -6,8 +6,8 @@ import Logo from "@/public/AN.jpg"
 
 function heroSection(){
     return (
-        <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-300 bg-center bg-blend-multiply flex items-center justify-center text-3xl relative`} id='home'>
-            <div className='flex flex-col items-center justify-center gap-4'>
+        <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-400 bg-center bg-blend-multiply flex items-center justify-center text-3xl relative`} id='home'>
+            <div className='flex flex-col items-center justify-center gap-6'>
                 <Image src={Logo} alt="Logo" className='size-28 md:size-48 rounded-full'/>
                 <h1 className='text-2xl md:text-4xl text-center text-text-50 font-extrabold w-full md:w-3/4'>Alamil and Napoles Wood & Metal Fabrication</h1>
             </div>
