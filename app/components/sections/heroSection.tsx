33eddea9048e0f6fefc673032fa78269187ccf56
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-12 16:41:06 +0800

    ``` No code changes made. ```

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { motion } from 'framer-motion'
 import React from 'react'
 import Image from 'next/image'  
 import Logo from "@/public/AN.jpg"
@@ -7,10 +8,14 @@ import Logo from "@/public/AN.jpg"
 function heroSection(){
     return (
         <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-400 bg-center bg-blend-multiply flex items-center justify-center text-3xl relative`} id='home'>
-            <div className='flex flex-col items-center justify-center gap-6'>
-                <Image src={Logo} alt="Logo" className='size-28 md:size-48 rounded-full'/>
-                <h1 className='text-2xl md:text-4xl text-center text-text-50 font-extrabold w-full md:w-3/4'>Alamil and Napoles Wood & Metal Fabrication</h1>
-            </div>
+            <motion.div 
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 2, scale: 1 }}
+                transition={{duration: 1, delay: .5, type: "spring"}}
+                className='flex flex-col items-center justify-center gap-6'>
+                    <Image src={Logo} alt="Logo" className='size-28 md:size-48 rounded-full'/>
+                    <h1 className='text-2xl md:text-4xl text-center text-text-50 font-extrabold w-full md:w-3/4'>Alamil and Napoles Wood & Metal Fabrication</h1>
+            </motion.div>
         </section>
     )
 }
