9c55e7b04f6b1594d9c8521d917cef7d30cd8fd3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-12 16:41:22 +0800

    Refactor services section: remove redundant background class for cleaner layout

--- app/components/sections/servicesSection.tsx
+++ app/components/sections/servicesSection.tsx
@@ -4,8 +4,8 @@ import ServicesCard from '../servicesCard'
 
 function servicesSection() {
     return (
-        <section className='py-8 px-4 bg-primary-50 flex flex-col items-center justify-center gap-6' id='services'>
-            <div className='flex flex-col items-center justify-center gap-4 max-w-[950px]'>
+        <section className='py-8 px-4 flex flex-col items-center justify-center gap-6' id='services'>
+            <div className='flex flex-col items-center justify-center gap-4'>
                 <h1 className='text-4xl md:text-5xl font-bold text-center'>Services We Provide</h1>
                 <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center items-center gap-4'>
                     {ServicesCardData.map((card, idx) => 
