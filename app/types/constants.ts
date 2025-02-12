1ded8666214817f381e0c178851249aebae067ff
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-12 16:41:52 +0800

    Add wood and metal images, and restructure data for improved categorization

--- app/types/constants.ts
+++ app/types/constants.ts
@@ -1,13 +1,45 @@
 import Cabinet from "@/public/carousel/cabinet.jpg"
 import Metal from "@/public/carousel/metal.jpg"
 import Painting from "@/public/carousel/painting.jpg"
-import Frame2 from "@/public/woodFrames/frame2.jpg"
 import Dogcage from "@/public/metal/dogcage.jpg"
 import { Wrench, Settings, ShoppingBasket, Facebook } from "lucide-react"
 import { carouselProps, servicesCardProps } from "./types"
+//Wood Works
 import Wood from "@/public/wood/wood.jpg"
 import Wood3 from "@/public/wood/wood3.jpg"
-import { title } from "process"
+import Wood4 from "@/public/wood/wood4.jpg"
+import Wood5 from "@/public/wood/wood5.jpg"
+import Wood6 from "@/public/wood/wood6.jpg"
+import Wood7 from "@/public/wood/wood7.jpg"
+import Wood8 from "@/public/wood/wood8.jpg"
+import Wood9 from "@/public/wood/wood9.jpg"
+import Wood10 from "@/public/wood/wood10.jpg"
+import Wood11 from "@/public/wood/wood11.jpg"
+import Wood12 from "@/public/wood/wood12.jpg"
+import Wood13 from "@/public/wood/wood13.jpg"
+import Wood14 from "@/public/wood/wood14.jpg"
+import Wood15 from "@/public/wood/wood15.jpg"
+import Wood18 from "@/public/wood/wood18.jpg"
+import Wood19 from "@/public/wood/wood19.jpg"
+
+//Metal Works
+import Metal1 from "@/public/metal/metal1.jpg"
+import Metal2 from "@/public/metal/metal2.jpg"
+import Metal3 from "@/public/metal/metal3.jpg"
+import Metal4 from "@/public/metal/metal4.jpg"
+import Metal5 from "@/public/metal/metal5.jpg"
+import Metal6 from "@/public/metal/metal6.jpg"
+
+//Wood Frames
+import Frame from "@/public/woodFrames/frame.jpg"
+import Frame1 from "@/public/woodFrames/frame1.jpg"
+import Frame2 from "@/public/woodFrames/frame2.jpg"
+import Frame4 from "@/public/woodFrames/frame4.jpg"
+import Frame5 from "@/public/woodFrames/frame5.jpg"
+import Frame6 from "@/public/woodFrames/frame6.jpg"
+import Frame7 from "@/public/woodFrames/frame7.jpg"
+import Frame8 from "@/public/woodFrames/frame8.jpg"
+
 
 // Datas
 export const navLinksHome = [
@@ -111,7 +143,7 @@ export const footerDataSocials = [
     }
 ]
 
-export const HomeAndOfficeImprovementData = [
+export const WoodWorksData = [
     {
         image: Wood,
         alt: "Bar top",
@@ -124,44 +156,170 @@ export const HomeAndOfficeImprovementData = [
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood4,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood5,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood6,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood,
+        image: Wood7,
         alt: "Bar top",
         title: "Bar top",
         description: "Bar top"
     },
     {
-        image: Wood3,
+        image: Wood8,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood9,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood10,
         alt: "chairs",
         description: "chairs"
     },
     {
-        image: Wood3,
+        image: Wood11,
         alt: "chairs",
         description: "chairs"
+    },
+    {
+        image: Wood12,
+        alt: "chairs",
+        description: "chairs"
+    },
+    {
+        image: Wood13,
+        alt: "chairs",
+        description: "chairs"
+    },
+    {
+        image: Wood14,
+        alt: "chairs",
+        description: "chairs"
+    },
+    {
+        image: Wood15,
+        alt: "chairs",
+        description: "chairs"
+    },
+    {
+        image: Wood18,
+        alt: "chairs",
+        description: "chairs"
+    },
+    {
+        image: Wood19,
+        alt: "chairs",
+        description: "chairs"
+    },
+]
+
+export const MetalWorksData = [
+    {
+        image: Dogcage,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },
+    {
+        image: Metal1,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },
+    {
+        image: Metal2,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },  
+    {
+        image: Metal3,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },  
+    {
+        image: Metal4,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },
+    {
+        image: Metal5,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },
+    {
+        image: Metal6,
+        alt: "Dog Cage",
+        title: "Dog Cage",
+        description: "Dog Cage"
+    },                   
+]
+
+export const WoodFramesData = [
+    {
+        image: Frame,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame1,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame2,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame4,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame5,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame6,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame7,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
+    },
+    {
+        image: Frame8,
+        alt: "Frame",
+        title: "Frame",
+        description: "Frame"
     },
 ]
