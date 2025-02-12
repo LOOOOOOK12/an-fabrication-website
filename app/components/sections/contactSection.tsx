809006ca0427777d24044715e14ff4b69dd710ce
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-12 16:40:58 +0800

    Enhance contact section: add decorative shapes for improved visual appeal

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -1,12 +1,22 @@
 "use client"
 
 import React from 'react'
+import Shape from '../shape'
 // import { footerDataSocials } from '@/app/types/constants'
 // import { Icon } from '../offersCard'
 
 function contactSection() {
     return (
-        <div className='py-8 px-4 flex flex-col gap-8 items-center justify-center h-screen' id='contactus'>
+        <div className='relative py-8 px-4 flex flex-col gap-8 items-center justify-center h-screen' id='contactus'>
+            <Shape
+                className={"-z-40 top-12 left-12 -rotate-12 rounded-lg size-32 lg:size-56 bg-accent-300"}
+            />
+            <Shape
+                className={"-z-40 bottom-0 left-0 -rotate-45 rounded-lg size-32 lg:size-56 bg-accent-200"}
+            />
+            <Shape
+                className={"-z-40 right-12 bottom-20 rotate-12 rounded-lg size-32 lg:size-64 bg-accent-600"}
+            />
             <div className='flex flex-col gap-4 items-center justify-center'>
                 <h1 className='text-3xl md:text-6xl font-bold text-center'>Want Something Built?  </h1>
                 <a href='mailto:lukealamil12@gmail.com' className='p-2 md:py-3 md:px-6 bg-accent-500 text-text-50 text-xl md:text-3xl font-semibold rounded-md hover:bg-accent-600 duration-100'>Contact Us!</a>
