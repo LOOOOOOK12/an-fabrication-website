0364e2c20986a0648757cba5d1b9ce8fd5175f0b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-14 16:26:09 +0800

    Refactor contact section: remove commented-out social media links for cleaner code

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -21,15 +21,6 @@ function contactSection() {
                 <h1 className='text-3xl md:text-6xl font-bold text-center'>Want Something Built?  </h1>
                 <a href='mailto:lukealamil12@gmail.com' className='p-2 md:py-3 md:px-6 bg-accent-500 text-text-50 text-xl md:text-3xl font-semibold rounded-md hover:bg-accent-600 duration-100'>Contact Us!</a>
             </div>
-            {/* <div className='flex flex-col gap-4 items-center justify-center'>
-                <h1 className='text-2xl md:text-4xl font-bold text-center'>You can also visit us in</h1>
-                <div className='flex items-center justify-center'>
-                    {footerDataSocials.map((socials,idx)=>
-                    <a key={idx} target='_blank' href={socials.href}>
-                        <Icon icon={socials.socialsIcons} size={50}/>
-                    </a>)}
-                </div>
-            </div> */}
         </div>
     )
 }
