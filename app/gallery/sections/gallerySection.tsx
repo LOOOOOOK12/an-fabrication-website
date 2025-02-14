a4c996dc2dad459326688f7cf88990d8a4d0c269
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-14 16:26:27 +0800

    Update gallery section: enhance layout with increased padding and adjust grid structure for better responsiveness

--- app/gallery/sections/gallerySection.tsx
+++ app/gallery/sections/gallerySection.tsx
@@ -4,10 +4,10 @@ import ImageHover from '@/app/components/imageHover';
 
 function GallerySection() {
     return (
-        <section className="flex flex-col gap-6 items-center justify-center py-28 px-4" id="gallery">
-            <div className="h-full">
-                <h1 className="text-5xl font-extrabold mb-6 text-center">Home and Office Improvement</h1>
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <section className="flex flex-col gap-12 items-center justify-center py-28 px-12" id="gallery">
+            <div className="w-full" id='homeAndOfficeImprovement'>
+                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Home and Office Improvement</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     {WoodWorksData.map((card, idx) => 
                         <ImageHover
                             key={idx}
@@ -19,9 +19,9 @@ function GallerySection() {
                     )}
                 </div>
             </div>
-            <div className="w-full">
-                <h1 className="text-5xl font-extrabold mb-6 text-center">Metal Works</h1>
-                <div className="grid grid-cols-2 md:grid-cols-5 grid-rows-2 gap-3">
+            <div className="w-full" id='metalWorks'>
+                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Metal Works</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {MetalWorksData.map((card, idx) =>
                     <ImageHover
                         key={idx}
@@ -33,9 +33,9 @@ function GallerySection() {
                 )}
                 </div>
             </div>
-            <div className="w-full">
-                <h1 className="text-5xl font-extrabold mb-6 text-center">Wood Frames</h1>
-                <div className="grid grid-cols-2 md:grid-cols-5 grid-rows-2 gap-3">
+            <div className="w-full" id='woodFrames'>
+                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Wood Frames</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {WoodFramesData.map((card, idx) =>
                     <ImageHover
                         key={idx}
