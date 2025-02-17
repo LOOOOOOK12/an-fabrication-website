5ec9a9ce111b75ccd4b656f450840742b0a7a663
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-17 20:13:09 +0800

    Update gallery section: adjust heading sizes and change grid to flex layout for improved responsiveness

--- app/gallery/sections/gallerySection.tsx
+++ app/gallery/sections/gallerySection.tsx
@@ -4,10 +4,10 @@ import ImageHover from '@/app/components/imageHover';
 
 function GallerySection() {
     return (
-        <section className="flex flex-col gap-12 items-center justify-center py-28 px-12" id="gallery">
+        <section className="flex flex-col gap-12 items-center justify-center py-28 px-6" id="gallery">
             <div className="w-full" id='homeAndOfficeImprovement'>
-                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Home and Office Improvement</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Home and Office Improvement</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
                     {WoodWorksData.map((card, idx) => 
                         <ImageHover
                             key={idx}
@@ -20,8 +20,8 @@ function GallerySection() {
                 </div>
             </div>
             <div className="w-full" id='metalWorks'>
-                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Metal Works</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Metal Works</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
                 {MetalWorksData.map((card, idx) =>
                     <ImageHover
                         key={idx}
@@ -34,8 +34,8 @@ function GallerySection() {
                 </div>
             </div>
             <div className="w-full" id='woodFrames'>
-                <h1 className="text-5xl font-extrabold mb-6 mt-6 text-center">Wood Frames</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Wood Frames</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
                 {WoodFramesData.map((card, idx) =>
                     <ImageHover
                         key={idx}
