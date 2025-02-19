08ae18a8c1b91108bdbb4d98659fabac531c71a4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-19 15:34:05 +0800

    Add DialogHeader to imageDialog for improved layout and presentation

--- app/components/imageDialog.tsx
+++ app/components/imageDialog.tsx
@@ -3,6 +3,7 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"  
@@ -16,7 +17,9 @@ function imageDialog({ src, alt, title, description}:TimageHoverProps) {
                 <Image src={src} alt={alt} className='w-full h-[400px] rounded-lg shadow-md brightness-50 hover:brightness-75 duration-100'/>
             </DialogTrigger>
             <DialogContent>
-                <Image src={src} alt={alt} className='w-full max-h-[400px] rounded-md'/>
+                <DialogHeader className=' max-h-[400px] w-full'>
+                    <Image src={src} alt={alt} className='w-full h-full object-cover rounded-md'/>
+                </DialogHeader>
                 <DialogTitle className='text-4xl'>{title}</DialogTitle>
                 <DialogDescription className='text-2xl'>{description}</DialogDescription>
             </DialogContent>
