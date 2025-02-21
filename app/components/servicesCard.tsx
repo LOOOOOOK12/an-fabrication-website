d456213cce35a88d2e7f7de2720f357ab9b6d8cb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-21 21:18:09 +0800

    Update servicesCard component height for improved layout consistency

--- app/components/servicesCard.tsx
+++ app/components/servicesCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 function servicesCard({title, image, alt, href}:servicesCardProps) {
     return (
-        <Link href={href} className='relative shadow-lg rounded-lg w-full h-full'>
+        <Link href={href} className='relative shadow-lg rounded-lg w-full h-[450px]'>
             <Image src={image} alt={alt} className='w-full h-full object-cover rounded-lg brightness-50 hover:brightness-75 duration-100'/>
             <div className="absolute bottom-2 right-2 w-full text-darkMode-colors-text text-2xl text-right font-bold">
                 <h1 className='text-text-50'>{title}</h1>
