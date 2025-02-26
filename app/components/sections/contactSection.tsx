8ba180e8dc56bf47b9704755999a5a42554500c1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 10:32:35 +0800

    Update contactSection component styles for improved design consistency and readability

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -7,17 +7,20 @@ function contactSection() {
     return (
         <div className='relative py-8 px-4 flex flex-col gap-8 items-center justify-center h-screen' id='contactus'>
             <Shape
-                className={"-z-40 top-12 left-12 -rotate-12 rounded-lg size-32 lg:size-56 bg-accent-300"}
+                className={"-z-40 top-12 left-12 -rotate-12 rounded-lg size-32 lg:size-56 bg-accent-500"}
             />
             <Shape
-                className={"-z-40 bottom-0 left-0 -rotate-45 rounded-lg size-32 lg:size-56 bg-accent-200"}
+                className={"-z-40 bottom-7 left-24 -rotate-45 rounded-lg size-32 lg:size-56 bg-secondary-200"}
             />
             <Shape
-                className={"-z-40 right-12 bottom-20 rotate-12 rounded-lg size-32 lg:size-64 bg-accent-600"}
+                className={"-z-40 right-12 bottom-20 rotate-12 rounded-lg size-32 lg:size-64 bg-primary"}
+            />
+            <Shape
+                className={"-z-40 top-8 right-32 -rotate-12 rounded-lg size-32 lg:size-44 bg-accent-600"}
             />
             <div className='flex flex-col gap-4 items-center justify-center'>
-                <h1 className='text-3xl md:text-6xl font-bold text-center'>Want Something Built?  </h1>
-                <a href='mailto:an.woodmetalfab@gmail.com' className='p-2 md:py-3 md:px-6 bg-accent-500 text-text-50 text-xl md:text-3xl font-semibold rounded-md hover:bg-accent-600 duration-100'>Contact Us!</a>
+                <h1 className='text-3xl md:text-6xl font-bold text-center'>Any Questions?</h1>
+                <a href='mailto:an.woodmetalfab@gmail.com' className='p-2 md:py-3 md:px-6 bg-secondary-800 text-text-50 text-xl md:text-3xl font-semibold rounded-md hover:bg-secondary-700 duration-100'>Contact Us!</a>
             </div>
         </div>
     )
