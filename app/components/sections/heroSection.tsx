f22e1b77e6f7490a59083e4c37bcc97375300bec
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 10:32:39 +0800

    Update heroSection component styles for improved design consistency and readability

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -7,14 +7,14 @@ import Logo from "@/public/logo.ico"
 
 function heroSection(){
     return (
-        <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-400 bg-center bg-blend-multiply flex items-center justify-center text-3xl relative`} id='home'>
+        <section className={`h-screen bg-[url(../public/wallpaper1.jpg)] bg-cover bg-primary-50 bg-center bg-blend-darken flex items-center justify-center text-3xl relative rounded-b-lg`} id='home'>
             <motion.div 
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 2, scale: 1 }}
                 transition={{duration: 1, delay: .5, type: "spring"}}
                 className='flex flex-col items-center justify-center gap-6'>
                     <Image src={Logo} alt="Logo" className='size-28 md:size-48 rounded-full'/>
-                    <h1 className='text-2xl md:text-4xl text-center text-text-50 font-extrabold w-full md:w-3/4'>Alamil and Napoles Wood & Metal Fabrication</h1>
+                    <h1 className='text-2xl md:text-4xl text-center text-zinc-50 font-extrabold w-full'>Alamil & Napoles Crafted Elements</h1>
             </motion.div>
         </section>
     )
