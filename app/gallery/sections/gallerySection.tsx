acbf626bd618ac5892527c318474c0464ecd00a6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 13:57:01 +0800

    Refactor GallerySection to update section titles and data sources for improved organization

--- app/gallery/sections/gallerySection.tsx
+++ app/gallery/sections/gallerySection.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { WoodWorksData, MetalWorksData, WoodFramesData } from '@/app/types/constants';
+import { CagesandFencesData, PictureFramesData, CabinetryData, BedsData, MetalWorksData } from '@/app/types/constants';
 import ImageDialog from '@/app/components/imageDialog';
 
 function GallerySection() {
     return (
         <section className="flex flex-col gap-12 items-center justify-center py-28 px-6" id="gallery">
-            <div className="w-full" id='homeAndOfficeImprovement'>
-                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Home and Office Improvement</h1>
+            <div className="w-full" id='cagesAndFences'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cages and Fences</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
-                    {WoodWorksData.map((card, idx) => 
+                    {CagesandFencesData.map((card, idx) => 
                         <ImageDialog
                             key={idx}
                             src={card.image} 
@@ -19,8 +19,22 @@ function GallerySection() {
                     )}
                 </div>
             </div>
-            <div className="w-full" id='metalWorks'>
-                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Metal Works</h1>
+            <div className="w-full" id='pictureFrames'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Picture Frames</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
+                    {PictureFramesData.map((card, idx) => 
+                        <ImageDialog
+                            key={idx}
+                            src={card.image} 
+                            alt={card.alt} 
+                            title={card.title || `Image ${idx + 1}`} 
+                            description={card.description} 
+                        />
+                    )}
+                </div>
+            </div>
+            <div className="w-full" id='gates'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Gates</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
                 {MetalWorksData.map((card, idx) =>
                     <ImageDialog
@@ -33,10 +47,10 @@ function GallerySection() {
                 )}
                 </div>
             </div>
-            <div className="w-full" id='woodFrames'>
-                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Wood Frames</h1>
+            <div className="w-full" id='cabinetry'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cabinetry</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
-                {WoodFramesData.map((card, idx) =>
+                {CabinetryData.map((card, idx) =>
                     <ImageDialog
                         key={idx}
                         src={card.image}
@@ -47,10 +61,24 @@ function GallerySection() {
                 )}
                 </div>
             </div>
-            <div>
-            <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cabinetry</h1>
+            <div className='w-full' id='cages'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cages</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
-                {WoodFramesData.map((card, idx) =>
+                {CagesandFencesData.map((card, idx) =>
+                    <ImageDialog
+                        key={idx}
+                        src={card.image}
+                        alt={card.alt} 
+                        title={card.title || `Image ${idx + 1}`} 
+                        description={card.description}
+                    />
+                )}
+                </div>
+            </div>
+            <div className='w-full' id='beds'>
+                <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Beds</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
+                {BedsData.map((card, idx) =>
                     <ImageDialog
                         key={idx}
                         src={card.image}
