2edb101f6b5ebd27ac3d98e9dd266714cd51017a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 10:32:19 +0800

    Update navigation component styles and logo for improved consistency

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,6 +6,7 @@ import { navLinks } from '../types/types'
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '@/public/logo.ico'
+import Dark from '@/public/dark.ico'
 import { Menu, X } from 'lucide-react'
 
 function Nav({ links }: navLinks) {
@@ -22,13 +23,13 @@ function Nav({ links }: navLinks) {
             transition={{ duration: 1.5, type:"spring", delay: 1 }}
             className='w-full fixed px-2 flex items-center justify-center top-2 z-50 bg-opacity-75'
         >
-            <nav className='w-full md:w-[850px] bg-primary-700 flex items-center justify-between text-text-50 p-3 shadow-lg'>
+            <nav className='w-full md:w-[850px] bg-background flex items-center justify-between p-2 shadow-2xl'>
                 <Link className='cursor-default text-3xl font-semibold size-10' href={"/"}>
-                    <Image src={Logo} alt="Logo" className='h-full w-full rounded-full'/>
+                    <Image src={Dark} alt="Logo" className='h-full w-full rounded-full'/>
                 </Link>
                 <div className='hidden md:flex gap-3'>
                     {links.map((link, idx) => 
-                        <a className='font-semibold hover:text-accent-200 hover:duration-100' key={idx} href={link.href}>{link.name}</a>
+                        <a className='font-semibold text-text-800 hover:duration-100' key={idx} href={link.href}>{link.name}</a>
                     )}
                 </div>
                 <button className='md:hidden' onClick={toggleMenu}>
@@ -42,10 +43,10 @@ function Nav({ links }: navLinks) {
                         animate={{opacity: 1, x: 0 }}
                         exit={{opacity: 0, x: 100 }}
                         transition={{ duration: .5, type:"spring", delay: .5 }}
-                        className='md:hidden absolute -top-2 right-0 h-[1200px] w-[70%] bg-primary-700 flex flex-col items-center text-text-50 p-2 shadow-lg'>
+                        className='md:hidden absolute -top-2 right-0 h-[1200px] w-[70%] bg-primary flex flex-col items-center text-text-50 p-2 shadow-lg'>
                             <X onClick={toggleMenu}/>
                         {links.map((link, idx) => 
-                            <a className='text-base hover:text-secondary-400 hover:duration-100 p-2' key={idx} href={link.href}>{link.name}</a>
+                            <a className='text-zinc-50 hover:text-zinc-500 duration-100 p-2' key={idx} href={link.href}>{link.name}</a>
                         )}
                     </motion.div>
                 )}
