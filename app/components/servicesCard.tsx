44a49f7e88958fb200ac44e5d18d540fac573f12
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 13:56:48 +0800

    Add placeholder attribute to servicesCard Image component for improved loading experience

--- app/components/servicesCard.tsx
+++ app/components/servicesCard.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 function servicesCard({title, image, alt, href}:servicesCardProps) {
     return (
         <Link href={href} className='relative shadow-lg rounded-3xl w-full h-[450px]'>
-            <Image src={image} alt={alt} className='w-full h-full object-cover rounded-lg brightness-50 hover:brightness-75 duration-100'/>
+            <Image placeholder='blur' src={image} alt={alt} className='w-full h-full object-cover rounded-lg brightness-50 hover:brightness-75 duration-100'/>
             <div className="absolute bottom-2 right-2 w-full text-darkMode-colors-text text-4xl text-right font-bold">
                 <h1 className='text-zinc-50'>{title}</h1>
             </div>
