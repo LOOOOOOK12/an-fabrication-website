51b4b52f0c5ffd46bb8b3135285217e5d8d411d2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-26 10:32:23 +0800

    Adjust offersCard component typography for improved readability

--- app/components/offersCard.tsx
+++ app/components/offersCard.tsx
@@ -10,8 +10,8 @@ function offersCard({icon, title, description}:offersCardProps) {
     return (
         <div className='flex flex-col items-center p-8 bg-background rounded-lg max-w-[300px] shadow-md'>
             <Icon icon={icon} size={50}/>
-            <h1 className='text-2xl md:text-4xl my-4 font-extrabold text-center'>{title}</h1>
-            <p className='text-lg md:text-xl text-text-400'>{description}</p>
+            <h1 className='text-2xl md:text-3xl my-4 font-extrabold text-center'>{title}</h1>
+            <p className='text-lg text-text-400'>{description}</p>
         </div>
     )
 }
