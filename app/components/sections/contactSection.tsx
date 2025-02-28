de95b57c087732d4bee49ab98fdc63e2d98ba441
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-28 09:58:17 +0800

    💄 Update contactSection shape sizes for improved layout consistency

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -6,17 +6,21 @@ import Shape from '../shape'
 function contactSection() {
     return (
         <div className='relative py-8 px-4 flex flex-col gap-8 items-center justify-center h-screen' id='contactus'>
+            {/* Left */}
             <Shape
-                className={"-z-40 top-12 left-12 -rotate-12 rounded-lg size-32 lg:size-56 bg-accent-500"}
+                className={"-z-40 top-12 left-12 -rotate-12 rounded-lg size-24  md:left-12 md:size-24 lg:size-56 bg-accent-500"}
             />
+            {/* Bottom Left */}
             <Shape
-                className={"-z-40 bottom-14 left-24 -rotate-45 rounded-lg size-32 lg:size-56 bg-secondary-200"}
+                className={"-z-40 bottom-14 left-24 -rotate-45 rounded-lg size-24 md:left-24 md:size-24 lg:size-56 bg-secondary-200"}
             />
+            {/* Bottom Right */}
             <Shape
-                className={"-z-40 right-12 bottom-20 rotate-12 rounded-lg size-32 lg:size-64 bg-primary"}
+                className={"-z-40 right-12 bottom-20 rotate-12 rounded-lg size-24 md:bottom-20 md:size-24 lg:size-64 bg-primary"}
             />
+            { /* Top Right */ }
             <Shape
-                className={"-z-40 top-8 right-20 -rotate-45 rounded-lg size-32 lg:size-44 bg-accent-600"}
+                className={"-z-40 top-8 right-20 -rotate-45 rounded-lg size-24 md:right-20 md:size-24 lg:size-44 bg-accent-600"}
             />
             <div className='flex flex-col gap-4 items-center justify-center'>
                 <h1 className='text-3xl md:text-6xl font-bold text-center'>Any Questions?</h1>
