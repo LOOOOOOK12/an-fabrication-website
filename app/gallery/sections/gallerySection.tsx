ffaf20d958e7c6ae5c181281c8d12e963fcde217
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2025-02-28 09:59:53 +0800

    💡  Remove 'Gates' section from Gallery for improved navigation clarity

--- app/gallery/sections/gallerySection.tsx
+++ app/gallery/sections/gallerySection.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { CagesandFencesData, PictureFramesData, CabinetryData, BedsData, MetalWorksData } from '@/app/types/constants';
+import { CagesandFencesData, PictureFramesData, CabinetryData, BedsData } from '@/app/types/constants';
 import ImageDialog from '@/app/components/imageDialog';
 
 function GallerySection() {
     return (
-        <section className="flex flex-col gap-4 items-center justify-center py-28 px-6" id="gallery">
+        <section className="flex flex-col gap-4 items-center justify-center py-20 px-6" id="gallery">
             <div className="w-full" id='cagesAndFences'>
                 <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cages and Fences</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
@@ -33,7 +33,7 @@ function GallerySection() {
                     )}
                 </div>
             </div>
-            <div className="w-full" id='gates'>
+            {/* <div className="w-full" id='gates'>
                 <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Gates</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
                 {MetalWorksData.map((card, idx) =>
@@ -46,7 +46,7 @@ function GallerySection() {
                     />
                 )}
                 </div>
-            </div>
+            </div> */}
             <div className="w-full" id='cabinetry'>
                 <h1 className="text-4xl md:text-5xl font-extrabold mb-6 mt-6 text-center">Cabinetry</h1>
                 <div className="flex flex-wrap items-center justify-center gap-4">
